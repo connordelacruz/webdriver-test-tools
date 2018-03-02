f594d7c275d8b6134eadb3a090beb64905cf9861
Connor de la Cruz <cdelacruz@sccadv.com>
2018-03-02 16:30:45 -0600

    Removed local copy of randomuser
    Using python package instead

--- webdriver_test_tools/__init__.py
+++ webdriver_test_tools/__init__.py
@@ -1,4 +1,4 @@
-from webdriver_test_tools.webdriver.support import expected_conditions as EC
+from .webdriver.support import expected_conditions as EC
 from . import test
-from .randomuser import RandomUser
 from . import test_module
+from randomuser import RandomUser
