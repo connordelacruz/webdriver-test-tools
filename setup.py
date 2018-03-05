267a1152f27f91411c60b535cb467db7bbfb79d5
Connor de la Cruz <cdelacruz@sccadv.com>
2018-03-05 12:05:17 -0600

    Added repo url to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
         name='webdriver_test_tools',
         version='0.1.0',
+        url='https://github.com/connordelacruz/webdriver-test-tools.git',
         packages=find_packages(),
         install_requires=[
             'selenium>=3.9',
