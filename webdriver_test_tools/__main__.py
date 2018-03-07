ade4246f26bb0b2e6e726047d5c197749254fe9a
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-07 16:51:39 -0600

    Added get_parser() which returns ArgumentParser for __main__
    Not yet implemented

--- webdriver_test_tools/__main__.py
+++ webdriver_test_tools/__main__.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+import argparse
 
-# TODO: parse args for init
+
+def get_parser():
+    """Returns ArgumentParser object for use with main()"""
+    parser = argparse.ArgumentParser()
+    # Argument for initializing
+    # TODO: optional filepath
+    parser.add_argument('-i', '--init', action='store_true', help='Initialize a new test project in the current directory')
+    return parser
+
+# TODO: main
