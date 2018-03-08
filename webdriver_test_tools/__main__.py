5953c4910ee218d5266d8b373a3fd16574c4e91e
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-08 08:39:33 -0600

    Added main() which parses args and handles appropriately

--- webdriver_test_tools/__main__.py
+++ webdriver_test_tools/__main__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import argparse
 
+from webdriver_test_tools.version import __version__
+from webdriver_test_tools.project import initialize
+
 
 def get_parser():
     """Returns ArgumentParser object for use with main()"""
@@ -8,6 +11,21 @@ def get_parser():
     # Argument for initializing
     # TODO: optional filepath
     parser.add_argument('-i', '--init', action='store_true', help='Initialize a new test project in the current directory')
+    # Print version number
+    parser.add_argument('-V', '--version', action='store_true', help='Print version number and exit')
     return parser
 
-# TODO: main
+
+def main():
+    """Parse command line arguments and handle appropriately"""
+    parser = get_parser()
+    args = parser.parse_args()
+    if args.version is not None and args.version:
+        print('webdriver_test_tools ' + __version__)
+        return
+    if args.init:
+        initialize.main()
+
+
+if __name__ == '__main__':
+    main()
