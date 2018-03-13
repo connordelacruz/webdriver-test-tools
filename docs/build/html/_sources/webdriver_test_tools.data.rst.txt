6a29fc6b64c82fd997035d5c3b32fcab29bb236d
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-13 11:01:58 -0500

    Moved RandomUser autodoc stuff to docstring in data.__init__

--- docs/build/html/_sources/webdriver_test_tools.data.rst.txt
+++ docs/build/html/_sources/webdriver_test_tools.data.rst.txt
@@ -8,11 +8,4 @@ Module contents
     :members:
     :undoc-members:
 
-    The python-randomuser package is installed as a convenience. For more information,
-    see `its documentation <http://connordelacruz.com/python-randomuser/>`_
-
-.. autoclass:: randomuser.randomuser.RandomUser
-    :members:
-    :undoc-members:
-
-    .. automethod:: __init__
+    
--- webdriver_test_tools/data/__init__.py
+++ webdriver_test_tools/data/__init__.py
@@ -1,2 +1,10 @@
-# TODO: add docstring with info on randomuser package
+"""
+The python-randomuser package is installed as a convenience. For more information, see `its documentation <http://connordelacruz.com/python-randomuser/>`_
+
+.. autoclass:: randomuser.randomuser.RandomUser
+    :members:
+    :undoc-members:
+
+    .. automethod:: __init__
+"""
 from randomuser import RandomUser
