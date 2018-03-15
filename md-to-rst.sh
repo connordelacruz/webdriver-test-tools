9a4dd0cf6027112e15eedf44b7192abc6cc56791
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-15 15:38:59 -0500

    Added link to test projects documentation to README
    Also removed TEST_PROJECTS.md since that documentation exists in docs/

--- md-to-rst.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Convert README.md and TEST_PROJECTS.md to .rst and output in docs/source/
-
-pandoc -f markdown -t rst -o ./docs/source/README.rst README.md
-pandoc -f markdown -t rst -o ./docs/source/TEST_PROJECTS.rst TEST_PROJECTS.md
