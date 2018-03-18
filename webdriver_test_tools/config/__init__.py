c72ed31427532ff7be85d24b19ee42cdfc8ca28f
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-18 15:05:33 -0500

    Added template config and moved browser config variables to BrowserConfig class

--- webdriver_test_tools/config/__init__.py
+++ webdriver_test_tools/config/__init__.py
@@ -1,3 +1,5 @@
 from .site import SiteConfig
 from .test import TestSuiteConfig
 from .webdriver import WebDriverConfig
+from .browser import BrowserConfig
+
--- webdriver_test_tools/project/templates/config/browser.py
+++ webdriver_test_tools/project/templates/config/browser.py
@@ -1,20 +1,13 @@
 # Configurations for what browser test cases to use
 
 from webdriver_test_tools.classes.webdriver_test_case import *
-
-class Browsers(object):
-    """Constants for browser short names. Used for mapping to classes in BROWSER_TEST_CLASSES"""
-    FIREFOX = FirefoxTestCase.SHORT_NAME
-    CHROME = ChromeTestCase.SHORT_NAME
-    SAFARI = SafariTestCase.SHORT_NAME
-    IE = IETestCase.SHORT_NAME
-    EDGE = EdgeTestCase.SHORT_NAME
+from webdriver_test_tools.config import browser
+from webdriver_test_tools.config.browser import Browsers
 
 
-class BrowserConfig(object):
+class BrowserConfig(browser.BrowserConfig):
     # Dictionary of usable test case classes indexed by the short name for that class (i.e. its command line argument)
     BROWSER_TEST_CLASSES = {
-        # Default to Chrome and Firefox since those aren't platform specific
         Browsers.FIREFOX: FirefoxTestCase,
         Browsers.CHROME: ChromeTestCase,
         # Browsers.SAFARI: SafariTestCase,
