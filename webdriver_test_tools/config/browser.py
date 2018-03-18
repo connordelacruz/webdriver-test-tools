f11e1ccc1ba161020dfa2c74d94b89ce51cf586b
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-18 14:11:35 -0500

    Added comments to config.browser

--- webdriver_test_tools/config/browser.py
+++ webdriver_test_tools/config/browser.py
@@ -1,8 +1,10 @@
-# TODO: document
+# Configurations for what browser test cases to use
+
 # TODO: make a template config for this for projects to override
 from webdriver_test_tools.classes.webdriver_test_case import *
 
 class Browsers(object):
+    """Constants for browser short names. Used for mapping to classes in BROWSER_TEST_CLASSES"""
     FIREFOX = FirefoxTestCase.SHORT_NAME
     CHROME = ChromeTestCase.SHORT_NAME
     SAFARI = SafariTestCase.SHORT_NAME
@@ -12,10 +14,11 @@ class Browsers(object):
 
 # Dictionary of usable test case classes indexed by the short name for that class (i.e. its command line argument)
 BROWSER_TEST_CLASSES = {
+    # Default to Chrome and Firefox since those aren't platform specific
     Browsers.FIREFOX: FirefoxTestCase,
     Browsers.CHROME: ChromeTestCase,
-    Browsers.SAFARI: SafariTestCase,
-    Browsers.IE: IETestCase,
-    Browsers.EDGE: EdgeTestCase,
+    # Browsers.SAFARI: SafariTestCase,
+    # Browsers.IE: IETestCase,
+    # Browsers.EDGE: EdgeTestCase,
 }
 
