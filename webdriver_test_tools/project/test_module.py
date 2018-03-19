ae6b439ed0af96dee3ade773c0c53eca4fb402e2
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-19 09:02:48 -0500

    test module uses project config.BrowserConfig for handling available browsers

--- webdriver_test_tools/project/test_module.py
+++ webdriver_test_tools/project/test_module.py
@@ -3,8 +3,6 @@ import argparse
 import unittest
 
 from webdriver_test_tools import config
-# TODO: remove import once config is implemented
-from webdriver_test_tools.classes.webdriver_test_case import BROWSER_TEST_CLASSES
 from webdriver_test_tools.project import test_loader, test_factory
 
 
@@ -14,34 +12,46 @@ def main(tests_module, config_module=None):
     :param tests_module: The module object for <test_project>.tests
     :param config_module: (Optional) The module object for <test_project>.config. Will use webdriver_test_tools.config if not specified
     """
-    parser = get_parser(config_module)
+    # Fall back on default config module if test doesn't supply one
+    if config_module is None:
+        config_module = config
+    # Older projects may not have the BrowserConfig class
+    browser_config = config_module.BrowserConfig if 'BrowserConfig' in dir(config_module) else config.BrowserConfig
+    # Parse arguments
+    parser = get_parser(browser_config)
     args = parser.parse_args()
-    # TODO: update to use config_module
+    # Handle --browser args
     if args.browser is None:
         browser_classes = None
     else:
-        browser_classes = [browser_class for browser_name, browser_class in BROWSER_TEST_CLASSES.items() if browser_name in args.browser]
+        browser_classes = [
+                browser_class for browser_name, browser_class in browser_config.BROWSER_TEST_CLASSES.items()
+                if browser_name in args.browser
+        ]
     test_name = args.test
     test_module_name = args.module
+    # Enable graceful Ctrl+C handling
     unittest.installHandler()
-    if config_module is None:
-        config_module = config
     # Load WebDriverTestCase subclasses from project tests
     tests = test_loader.load_project_tests(tests_module, test_module_name)
     # Generate browser test cases from the loaded WebDriverTestCase classes
     browser_test_suite = test_factory.generate_browser_test_suite(tests, browser_classes, test_name)
+    # Get configured test runner and run suite
     test_runner = config_module.TestSuiteConfig.get_runner()
     test_runner.run(browser_test_suite)
 
 
-def get_parser(config_module=None):
-    """Returns the ArgumentParser object for use with main()"""
+def get_parser(browser_config=None):
+    """Returns the ArgumentParser object for use with main()
+
+    :param browser_config: (Optional) BrowserConfig class for the project. Defaults to webdriver_test_tools.config.BrowserConfig if unspecified
+    """
     parser = argparse.ArgumentParser()
     # Use default config if module is None or doesn't contain BrowserConfig class
-    if config_module is None or not 'BrowserConfig' in dir(config_module):
-        config_module = config
+    if browser_config is None:
+        browser_config = config.BrowserConfig
     # Arguments for specifying browser to use
-    browser_choices = [k for k in config_module.BrowserConfig.BROWSER_TEST_CLASSES]
+    browser_choices = [k for k in browser_config.BROWSER_TEST_CLASSES]
     parser.add_argument('-b', '--browser', nargs='+', choices=browser_choices, help='Run tests only in the specified browsers')
     # Arguments for specifying what test to run
     parser.add_argument('-t', '--test', help='Run a specific test case class or function', metavar='TestCase[.test_method]')
