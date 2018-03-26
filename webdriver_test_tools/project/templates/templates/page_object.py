3ad54c1a8ac437093df2fdcedb5a7cfa3f654bc1
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-26 09:11:15 -0500

    Updated templates
    - Removed TemplatePage.Input class since it's moved to FormObject
    - Removed redundant comments

--- webdriver_test_tools/project/templates/templates/page_object.py
+++ webdriver_test_tools/project/templates/templates/page_object.py
@@ -1,17 +1,10 @@
-# Page object
-
-# Imports
-# ----------------------------------------------------------------
 from webdriver_test_tools.classes.base_page import BasePage
 from webdriver_test_tools.webdriver import actions
 from selenium.webdriver.common.by import By
 
-# Page Classes
-# ----------------------------------------------------------------
 
 class TemplatePage(BasePage):
-    # Locators
-    # --------------------------------
+
     class Locator(object):
         """WebDriver locator tuples for any elements that will need to be accessed by
         this page object.
@@ -22,21 +15,8 @@ class TemplatePage(BasePage):
         """
         pass
 
-    # Input Names
-    # --------------------------------
-    class Input(object):
-        """Put the name attributes of inputs here (if applicable, otherwise the Input
-        subclass is not necessary)
-
-        :Example:
-
-            SOME_INPUT = 'someInput'
-        """
-        pass
-
 
     # Page Functions
     # --------------------------------
 
-    """Put functions that interact with page elements here."""
 
