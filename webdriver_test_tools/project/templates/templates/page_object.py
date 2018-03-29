3a807e64a7482cecd518f49bb54c4c793ca42f59
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-03-29 08:34:07 -0500

    Tweaked project template comments

--- webdriver_test_tools/project/templates/templates/page_object.py
+++ webdriver_test_tools/project/templates/templates/page_object.py
@@ -1,5 +1,5 @@
 from webdriver_test_tools.classes.base_page import BasePage
-from webdriver_test_tools.webdriver import actions
+from webdriver_test_tools.webdriver import actions, locate
 from selenium.webdriver.common.by import By
 
 
@@ -8,15 +8,10 @@ class TemplatePage(BasePage):
     class Locator(object):
         """WebDriver locator tuples for any elements that will need to be accessed by
         this page object.
-
-        :Example:
-
-            SOME_ELEMENT = (By.ID, 'some-element')
         """
         pass
 
 
     # Page Functions
-    # --------------------------------
 
 
