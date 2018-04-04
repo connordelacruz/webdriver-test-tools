c8eeda0ec7289d3533eeaa91aaba8105f872c2a0
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-04-04 16:49:57 -0500

    Added metadata to setup.py

--- setup.py
+++ setup.py
@@ -4,29 +4,69 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-# Get __version__
+# Get __version__ and __devstatus__
 with open('./webdriver_test_tools/version.py') as f:
     exec(f.read())
 
+
+# Installation
+
+install_requires = [
+    'selenium>=3.9',
+    'colour-runner>=0.0.5',
+    'randomuser>=1.2.0',
+    'py-loremipsum>=1.0.0',
+    'Jinja2>=2.9.5',
+]
+
+entry_points = {
+    'console_scripts': ['webdriver_test_tools = webdriver_test_tools.__main__:main']
+}
+
+# Metadata
+
+CLASSIFIERS = '''
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Environment :: Console
+License :: OSI Approved :: MIT License
+Natural Language :: English
+Intended Audience :: Developers
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
+Topic :: Software Development :: Libraries
+Topic :: Software Development :: Testing
+Topic :: Software Development :: Quality Assurance
+Topic :: Utilities
+{}
+'''.format(__devstatus__)
+
+url = 'https://github.com/connordelacruz/webdriver-test-tools/'
+
+download_url = url + 'archive/{}.tar.gz'.format(__version__)
+
+project_urls = {
+    'Documentation': 'http://connordelacruz.com/webdriver-test-tools/',
+    'Source': url,
+}
+
+
 setup(
-        name='webdriver_test_tools',
-        version=__version__,
-        description='Tools for using Selenium WebDriver with Python unit testing',
-        long_description=readme(),
-        url='https://github.com/connordelacruz/webdriver-test-tools',
-        download_url='https://github.com/connordelacruz/webdriver-test-tools/archive/{}.tar.gz'.format(__version__),
-        author='Connor de la Cruz',
-        author_email='connor.c.delacruz@gmail.com',
-        license='MIT',
-        packages=find_packages(),
-        entry_points={
-            'console_scripts': ['webdriver_test_tools = webdriver_test_tools.__main__:main']
-        },
-        install_requires=[
-            'selenium>=3.9',
-            'colour-runner>=0.0.5',
-            'randomuser>=1.2.0',
-            'py-loremipsum>=1.0.0',
-            'Jinja2>=2.9.5',
-            ]
-        )
+    name='webdriver_test_tools',
+    version=__version__,
+    description='Tools for using Selenium WebDriver with Python unit testing',
+    long_description=readme(),
+    url=url,
+    download_url=download_url,
+    project_urls=project_urls,
+    author='Connor de la Cruz',
+    author_email='connor.c.delacruz@gmail.com',
+    license='MIT',
+    classifiers=[x for x in CLASSIFIERS.split("\n") if x],
+    packages=find_packages(),
+    entry_points=entry_points,
+    install_requires=install_requires,
+)
--- webdriver_test_tools/version.py
+++ webdriver_test_tools/version.py
@@ -1,1 +1,4 @@
 __version__ = '0.14.2'
+
+# Development Status classifier
+__devstatus__ = 'Development Status :: 2 - Pre-Alpha'
