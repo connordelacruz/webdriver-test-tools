7fc351ddc4e2b625dfa3f21f4c94d66723b107ad
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-04-06 15:19:33 -0500

    Added initial config file for BrowserStack support
    - Created config.browserstack module
    - Added BrowserStackConfig class with values for enabling, credentials,
    and method for retrieving command executor url

---
+++ webdriver_test_tools/config/browserstack.py
@@ -0,0 +1,26 @@
+
+
+# TODO: document when everything is in place
+class BrowserStackConfig(object):
+    """Configurations for BrowserStack testing"""
+
+    # Set to True to enable BrowserStack testing
+    ENABLE = False
+    # Account username. Projects will need to override this
+    USERNAME = None
+    # Access key. Projects will need to override this
+    ACCESS_KEY = None
+
+
+    # Methods
+
+    @classmethod
+    def get_command_executor(cls):
+        """Returns the command executor URL"""
+        # TODO: exception if auth stuff not set
+        url_format = 'http://{USERNAME}:{ACCESS_KEY}@hub.browserstack.com:80/wd/hub'
+        command_executor = url_format.format(USERNAME=cls.USERNAME, ACCESS_KEY=cls.ACCESS_KEY)
+        return command_executor
+
+
+
