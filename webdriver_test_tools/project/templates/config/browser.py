92b0249b703e19ea3bf2c210e25b5a4877f66e34
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-04-06 08:54:32 -0500

    Updated template test case and browser config
    - template test case now imports all webdriver_test_case classes
    - Added Browsers.CHROME_MOBILE to template BrowserConfig class (disabled
    by default)

--- webdriver_test_tools/project/templates/config/browser.py
+++ webdriver_test_tools/project/templates/config/browser.py
@@ -5,12 +5,18 @@ from webdriver_test_tools.config import browser
 
 
 class BrowserConfig(browser.BrowserConfig):
-    # Dictionary of usable test case classes indexed by the short name for that class (i.e. its command line argument)
+    """Configurations for what browsers to generate tests for
+
+    :var BrowserConfig.BROWSER_TEST_CLASSES: Dictionary of browser test case classes to
+        use indexed by the short name for that class (i.e. its command line argument).
+        Default to Chrome and Firefox since they're not platform specific
+    """
     BROWSER_TEST_CLASSES = {
         Browsers.FIREFOX: FirefoxTestCase,
         Browsers.CHROME: ChromeTestCase,
         # Browsers.SAFARI: SafariTestCase,
         # Browsers.IE: IETestCase,
         # Browsers.EDGE: EdgeTestCase,
+        # Browsers.CHROME_MOBILE: ChromeMobileTestCase,
     }
 
