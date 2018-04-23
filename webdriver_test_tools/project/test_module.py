a2816662a35ff8da5cff05de00d00c66d3e41f3c
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-04-23 15:44:44 -0500

    Commented out currently unused import in project.test_module

--- webdriver_test_tools/project/test_module.py
+++ webdriver_test_tools/project/test_module.py
@@ -2,7 +2,7 @@
 import argparse
 import unittest
 import textwrap
-import warnings
+# import warnings
 from blessings import Terminal
 
 from webdriver_test_tools import config
@@ -190,7 +190,7 @@ def run_tests(tests_module, config_module, browser_classes=None, test_class_map=
     browser_test_suite = test_factory.generate_browser_test_suite(tests, browser_classes, test_class_map, config_module, browserstack)
     # Get configured test runner and run suite
     test_runner = config_module.TestSuiteConfig.get_runner()
-    # TODO: handle deprecation warnings after tests finish
+    # TODO: handle deprecation warnings after tests finish?
     # warnings.simplefilter('always', DeprecationWarning)
     test_runner.run(browser_test_suite)
     # Link to BrowserStack automation dashboard if applicable
