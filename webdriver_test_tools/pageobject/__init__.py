4c796fbefefdbe0b46a9c13c29d2408b5315690c
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-05-01 16:18:01 -0500

    Copied base_page and page_object_prototypes to pageobject subpackage
    Currently not yet implemented
    
    - Copied classes.base_page to pageobject.base
    - Copied classes.page_object_prototypes to pageobject.prototypes

---
+++ webdriver_test_tools/pageobject/__init__.py
@@ -0,0 +1,3 @@
+from .base import *
+from . import prototypes
+
---
+++ webdriver_test_tools/pageobject/base.py
@@ -0,0 +1,47 @@
+# Base class for page objects
+
+
+class BasePage(object):
+    """Base class for page objects"""
+
+    class Locator(object):
+        """WebDriver locator tuples for any elements that will need to be accessed by
+        this page object.
+
+        :Example:
+
+            ``SOME_ELEMENT = (By.ID, 'some-element')``
+        """
+        pass
+
+    def __init__(self, driver):
+        """Initialize page object
+
+        :param driver: Selenium WebDriver object
+        """
+        self.driver = driver
+
+
+    def find_element(self, locator):
+        """Returns a WebElement object based on the locator tuple passed
+
+        Shorthand for ``self.driver.find_element(*locator)``
+
+        :param locator: WebDriver locator tuple in the format (By.<attr>, <locator string>)
+
+        :rtype: WebElement
+        """
+        return self.driver.find_element(*locator)
+
+
+    def find_elements(self, locator):
+        """Returns a list of WebElement objects based on the locator tuple passed
+
+        Shorthand for ``self.driver.find_elements(*locator)``
+
+        :param locator: WebDriver locator tuple in the format (By.<attr>, <locator string>)
+
+        :rtype: List of WebElement
+        """
+        return self.driver.find_elements(*locator)
+
