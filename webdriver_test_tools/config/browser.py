5671fcc268f270f7f1c2fb4121a36abdbc161025
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-05-07 15:12:11 -0500

    Updated docstrings for config.browser and config.browserstack
    Docstrings and corresponding documentation now link to testcase.config

--- webdriver_test_tools/config/browser.py
+++ webdriver_test_tools/config/browser.py
@@ -1,3 +1,7 @@
+"""
+This module imports the :class:`BrowserConfig <webdriver_test_tools.testcase.config.BrowserConfig>`
+class for compatibility with legacy test projects. See the documentation for
+:mod:`webdriver_test_tools.testcase.config` for details.
+"""
 from webdriver_test_tools.testcase.config import BrowserConfig
 
-# TODO: deprecated
--- webdriver_test_tools/config/browserstack.py
+++ webdriver_test_tools/config/browserstack.py
@@ -1,3 +1,7 @@
+"""
+This module imports the :class:`BrowserStackConfig <webdriver_test_tools.testcase.config.BrowserStackConfig>`
+class for compatibility with legacy test projects. See the documentation for
+:mod:`webdriver_test_tools.testcase.config` for details.
+"""
 from webdriver_test_tools.testcase.config import BrowserStackConfig
 
-# TODO: deprecated
