6cdf6b07a59cc1a262069353e63deb2e2dfb849a
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-05-08 15:53:01 -0500

    Removed deprecated classes subpackage

--- webdriver_test_tools/classes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Test framework classes"""
-
-from . import base_page
-from . import webdriver_test_case
-from . import page_object_prototypes
-
--- webdriver_test_tools/classes/page_object_prototypes.py
+++
@@ -1,8 +0,0 @@
-"""This module has been moved to the :doc:`webdriver_test_tools.pageobject`
-subpackage. This module imports * from webdriver_test_tools.pageobject.prototypes
-for legacy purposes
-"""
-# TODO: deprecated
-from webdriver_test_tools.pageobject.prototypes import *
-
-
