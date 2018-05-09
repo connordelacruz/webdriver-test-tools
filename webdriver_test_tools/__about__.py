3dc8c4e861b3e7a3103ba2ed1012bad3348ed895
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-05-09 16:43:50 -0500

    setup.py now gets metadata from __about__.py

--- webdriver_test_tools/__about__.py
+++ webdriver_test_tools/__about__.py
@@ -1,16 +1,19 @@
 
 __all__ = [
-    '__title__', '__summary__', '__url__', '__version__',
-    '__devstatus__', '__selenium__', '__author__', '__email__',
-    '__license__',
+    '__title__', '__summary__', '__version__', '__devstatus__',
+    '__url__', '__documentation__', '__download__', '__selenium__',
+    '__author__', '__email__', '__license__',
 ]
 
 __title__ = 'webdriver_test_tools'
 __summary__ = 'A front-end testing framework using Selenium WebDriver and Python'
-__url__ = 'https://github.com/connordelacruz/webdriver-test-tools/'
 
 __version__ = '0.30.0'
 __devstatus__ = 'Development Status :: 3 - Alpha'
+
+__url__ = 'https://github.com/connordelacruz/webdriver-test-tools/'
+__documentation__ = 'http://connordelacruz.com/webdriver-test-tools/'
+__download__ = __url__ + 'archive/{}.tar.gz'.format(__version__)
 
 __selenium__ = '3.11.0'
 
