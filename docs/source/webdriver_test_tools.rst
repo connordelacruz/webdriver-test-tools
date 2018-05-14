a3ff8e1b14b10cee843f63b7de32a04f094c378d
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-05-14 16:38:54 -0500

    Moved root package toctree to __init__ docstring

--- docs/source/webdriver_test_tools.rst
+++ docs/source/webdriver_test_tools.rst
@@ -6,16 +6,3 @@ webdriver\_test\_tools API
     :undoc-members:
     :show-inheritance:
 
-Subpackages
------------
-
-.. toctree::
-
-    webdriver_test_tools.cmd
-    webdriver_test_tools.config
-    webdriver_test_tools.data
-    webdriver_test_tools.pageobject
-    webdriver_test_tools.project
-    webdriver_test_tools.testcase
-    webdriver_test_tools.webdriver
-
--- webdriver_test_tools/__init__.py
+++ webdriver_test_tools/__init__.py
@@ -1,5 +1,20 @@
+# TODO: split toctree into primary and internal?
 """
 |release|
+
+Subpackages
+-----------
+
+.. toctree::
+
+    webdriver_test_tools.cmd
+    webdriver_test_tools.config
+    webdriver_test_tools.data
+    webdriver_test_tools.pageobject
+    webdriver_test_tools.project
+    webdriver_test_tools.testcase
+    webdriver_test_tools.webdriver
+
 """
 from . import config
 from . import data
