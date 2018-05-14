fa73cf50be421d524de944c691b811ff4406d5fb
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-05-14 16:25:53 -0500

    Moved toctrees for project and testcase to their respective __init__ docstrings

--- webdriver_test_tools/testcase/__init__.py
+++ webdriver_test_tools/testcase/__init__.py
@@ -1,4 +1,26 @@
-"""Web driver test case classes"""
+"""Web driver test case classes.
+
+Main Modules
+------------
+
+.. toctree::
+
+   webdriver_test_tools.testcase.webdriver
+   webdriver_test_tools.testcase.browsers
+
+
+Browser Test Case Modules
+-------------------------
+
+.. toctree::
+   webdriver_test_tools.testcase.chrome
+   webdriver_test_tools.testcase.edge
+   webdriver_test_tools.testcase.firefox
+   webdriver_test_tools.testcase.ie
+   webdriver_test_tools.testcase.safari
+
+"""
 
 from .webdriver import WebDriverTestCase, WebDriverMobileTestCase
 from .browsers import Browsers
+
