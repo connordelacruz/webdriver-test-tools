ed2cf730b3c13d7e52200af3c5b462fbd0b6ed21
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-05-14 10:05:36 -0500

    Added autoclass documentation to pageobject.prototypes docstring

--- docs/source/webdriver_test_tools.pageobject.rst
+++ docs/source/webdriver_test_tools.pageobject.rst
@@ -9,9 +9,9 @@ webdriver\_test\_tools.pageobject package
 .. toctree::
 
    webdriver_test_tools.pageobject.base
+   webdriver_test_tools.pageobject.prototypes
    webdriver_test_tools.pageobject.form
    webdriver_test_tools.pageobject.modal
    webdriver_test_tools.pageobject.nav
-   webdriver_test_tools.pageobject.prototypes
    webdriver_test_tools.pageobject.webpage
 
--- webdriver_test_tools/pageobject/prototypes.py
+++ webdriver_test_tools/pageobject/prototypes.py
@@ -2,6 +2,35 @@
 
 These classes define common operations and attributes for various common components.
 These can be subclassed in test projects if useful.
+
+Prototype Classes
+=================
+
+.. autoclass:: webdriver_test_tools.pageobject.webpage.WebPageObject
+    :members:
+    :undoc-members:
+    :noindex:
+
+.. autoclass:: webdriver_test_tools.pageobject.nav.NavObject
+    :members:
+    :undoc-members:
+    :noindex:
+
+.. autoclass:: webdriver_test_tools.pageobject.nav.CollapsibleNavObject
+    :members:
+    :undoc-members:
+    :noindex:
+
+.. autoclass:: webdriver_test_tools.pageobject.form.FormObject
+    :members:
+    :undoc-members:
+    :noindex:
+
+.. autoclass:: webdriver_test_tools.pageobject.modal.ModalObject
+    :members:
+    :undoc-members:
+    :noindex:
+
 """
 
 from .webpage import WebPageObject
