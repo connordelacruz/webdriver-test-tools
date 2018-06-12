3533b6d709c584134f69a6fd1ed024e9a248af87
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-06-12 12:52:49 -0500

    Tweaked updage-gh-page.sh to push to github instead of origin

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Update gh-pages branch using docs/build/html/
 
-git subtree push --prefix docs/build/html origin gh-pages
+git subtree push --prefix docs/build/html github gh-pages
