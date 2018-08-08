4a44b2fefac8e187b19413b2a9488f75dd30dc91
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-08-08 13:21:11 -0500

    Updated example_project.rst to reflect test detection changes

--- docs/example/example-project/example_package/tests/__init__.py
+++ docs/example/example-project/example_package/tests/__init__.py
@@ -1,2 +1,7 @@
-# Import test modules here so example_package.__main__ can detect them
-from . import homepage
+import pkgutil
+
+# Detect and import test modules dynamically
+__path__ = pkgutil.extend_path(__path__, __name__)
+for importer, module, ispkg in pkgutil.walk_packages(path=__path__,prefix=__name__+'.'):
+    __import__(module)
+
