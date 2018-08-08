f8fb4b1e5066b6a23e5e488a637c807e465b5cfc
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-08-08 13:46:21 -0500

    Rebuilt docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '1.6.1-alpha',
+    VERSION: '1.7.0-alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
