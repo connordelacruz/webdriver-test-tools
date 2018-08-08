90da5d1c756b9e4f1ad7a9643af59cf26ecde05c
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-08-08 15:01:54 -0500

    Bumped version to 1.7.1 and rebuilt docs.

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '1.7.0-alpha',
+    VERSION: '1.7.1-alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
