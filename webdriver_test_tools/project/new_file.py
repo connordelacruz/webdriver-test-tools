551328e45e74912ba68955fc7ee36724b72a5d4a
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-08-13 15:38:05 -0500

    Output improvements
    - Added success message for new command
    - Added FileUtilError class, which is raised when one a function in
    common.files encounters an error
    - create_file_from_template() now raises a FileUtilError if overwrite is
    False and the target file already exists
    - new_file() now only prints error message if
    create_file_from_template() raises a FileUtilError (rather than printing
    the whole stacktrace)

--- webdriver_test_tools/project/new_file.py
+++ webdriver_test_tools/project/new_file.py
@@ -65,6 +65,8 @@ def new_file(test_package_path, test_package, file_type, module_name, class_name
     :param module_name: Filename to use for the new python module
     :param class_name: Name to use for the initial test class
     :param description: (Optional) Description to use in the docstring of the initial class
+    :param force: (Default: False) If True, force overwrite if a file with the
+        same name already exists
     """
     template_file, target_dir = validate_file_type(file_type)
     target_path = os.path.join(test_package_path, target_dir)
@@ -83,10 +85,16 @@ def new_file(test_package_path, test_package, file_type, module_name, class_name
         'class_name': class_name,
         'description': description,
     }
-    # TODO: catch exception if file exists and print error message
-    create_file_from_template(TEMPLATE_PATH, target_path, template_file, context,
-                              target_filename=module_name, overwrite=force)
-    # TODO: output new file path on success
+    try:
+        new_file_path = create_file_from_template(
+            TEMPLATE_PATH, target_path, template_file, context,
+            target_filename=module_name, overwrite=force
+        )
+    except FileUtilError as e:
+        cmd.print_exception(e)
+    # Output new file path on success
+    print(cmd.COLORS['success']('File created:'))
+    print(cmd.COLORS['success'](new_file_path))
 
 # TODO: main function for prompts?
 
