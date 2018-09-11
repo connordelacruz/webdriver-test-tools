d17d2dc4c2d964ff7f9b931647a8e091fb568f8b
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-09-11 13:34:18 -0500

    Added cleaner Ctrl + C handling and various internal changes
    - new_file() no longer handles validation. main() validates all values
    before calling new_file() and is the only public-facing entry point, so
    further validation is redundant
    - new_file() now returns the filepath to the new file
    - Moved 'file created' output to main() since it acts as a command line
    interface for new_file()
    - Added cleaner Ctrl + C handling to new_file.main() using the approach
    from initialize.main()

--- webdriver_test_tools/project/new_file.py
+++ webdriver_test_tools/project/new_file.py
@@ -1,5 +1,6 @@
 """Functions for creating a new test/page module."""
 
+import sys
 from webdriver_test_tools import cmd
 from webdriver_test_tools.common.files import *
 from webdriver_test_tools.project import templates
@@ -21,7 +22,6 @@ DIRECTORY_MAP = {
 }
 
 
-# TODO: convert to cmd.validate_choice?
 def validate_file_type(file_type):
     """Validate file type and return the corresponding template filename and
     target directory
@@ -66,6 +66,10 @@ def new_file(test_package_path, test_package, file_type, module_name, class_name
              description=None, force=False):
     """Create a new project file
 
+    This method assumes parameters have been validated. :func:`main()
+    <webdriver_test_tools.project.new_file.main()>` handles input validation
+    before calling this function
+
     :param test_package_path: The root directory of the test package
     :param test_package: The python package name of the test package
     :param file_type: The type of file to create. Valid file types are stored
@@ -75,32 +79,22 @@ def new_file(test_package_path, test_package, file_type, module_name, class_name
     :param description: (Optional) Description to use in the docstring of the initial class
     :param force: (Default: False) If True, force overwrite if a file with the
         same name already exists
+
+    :return: Path of the new file
     """
     template_file, target_dir = validate_file_type(file_type)
     target_path = os.path.join(test_package_path, target_dir)
-    # TODO: use main() as entry point, assume any input will be validated before new_file() call
-    # Validate module_name (and append .py if not already present)
-    module_name = cmd.validate_module_filename(module_name)
-    # Validate class_name
-    class_name = cmd.validate_class_name(class_name)
-    # Validate description name (None is considered valid)
-    description = validate_description(description)
     context = {
         'test_package': test_package,
         'module_name': module_name,
         'class_name': class_name,
         'description': description,
     }
-    new_file_path = create_file_from_template(
+    return create_file_from_template(
         TEMPLATE_PATH, target_path, template_file, context,
         target_filename=module_name, overwrite=force
     )
-    # Output new file path on success
-    print(cmd.COLORS['success']('\nFile created.'))
-    print(new_file_path)
 
-
-# TODO: main function for prompts?
 
 def main(test_package_path, test_package,
          file_type=None, module_name=None, class_name=None,
@@ -138,45 +132,55 @@ def main(test_package_path, test_package,
     :param force: (Default: False) If True, force overwrite if a file with the
         same name already exists
     """
-    # TODO: add Ctrl + C handling from initialize.main()?
-    # if module_name and class_name are set, use defaults for description and force
-    if module_name and class_name and description is None:
-        description = ''
-    _validate_file_type = cmd.validate_choice(
-        ['test','page'], shorthand_choices={'t': 'test', 'p': 'page'}
-    )
-    validated_file_type = cmd.prompt(
-        '[t]est/[p]age',
-        'Create a new test case or page object?',
-        validate=_validate_file_type,
-        parsed_input=file_type
-    )
-    validated_module_name = cmd.prompt(
-        'Module file name',
-        'Enter a file name for the new {} module'.format(validated_file_type),
-        validate=cmd.validate_module_filename,
-        parsed_input=module_name
-    )
-    class_type = 'test case' if validated_file_type == 'test' else 'page object'
-    validated_class_name = cmd.prompt(
-        '{} class name'.format(class_type.capitalize()),
-        'Enter a name for the initial {} class'.format(class_type),
-        validate=cmd.validate_class_name,
-        parsed_input=class_name
-    )
-    validated_description = cmd.prompt(
-        'Description',
-        '(Optional) Enter description of the new {} class'.format(class_type),
-        validate=validate_description,
-        default='',
-        parsed_input=description
-    )
-
-    new_file(
-        test_package_path, test_package,
-        file_type=validated_file_type, module_name=validated_module_name,
-        class_name=validated_class_name, description=validated_description,
-        force=force
-    )
+    new_file_start = False
+    try:
+        # if module_name and class_name are set, use defaults for description and force
+        if module_name and class_name and description is None:
+            description = ''
+        _validate_file_type = cmd.validate_choice(
+            ['test','page'], shorthand_choices={'t': 'test', 'p': 'page'}
+        )
+        validated_file_type = cmd.prompt(
+            '[t]est/[p]age',
+            'Create a new test case or page object?',
+            validate=_validate_file_type,
+            parsed_input=file_type
+        )
+        validated_module_name = cmd.prompt(
+            'Module file name',
+            'Enter a file name for the new {} module'.format(validated_file_type),
+            validate=cmd.validate_module_filename,
+            parsed_input=module_name
+        )
+        class_type = 'test case' if validated_file_type == 'test' else 'page object'
+        validated_class_name = cmd.prompt(
+            '{} class name'.format(class_type.capitalize()),
+            'Enter a name for the initial {} class'.format(class_type),
+            validate=cmd.validate_class_name,
+            parsed_input=class_name
+        )
+        validated_description = cmd.prompt(
+            'Description',
+            '(Optional) Enter description of the new {} class'.format(class_type),
+            validate=validate_description,
+            default='',
+            parsed_input=description
+        )
+        new_file_start = True
+        new_file_path = new_file(
+            test_package_path, test_package,
+            file_type=validated_file_type, module_name=validated_module_name,
+            class_name=validated_class_name, description=validated_description,
+            force=force
+        )
+        # Output new file path on success
+        print(cmd.COLORS['success']('\nFile created.'))
+        print(new_file_path)
+    except KeyboardInterrupt:
+        print('')
+        if new_file_start:
+            msg = 'File creation was cancelled mid-operation.'
+            print(cmd.COLORS['warning'](msg))
+        sys.exit()
 
 
