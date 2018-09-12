4ae7377e912ec2118406cf376c56675771630d22
Connor de la Cruz <connor.c.delacruz@gmail.com>
2018-09-12 14:23:52 -0500

    Added docstring to project.cmd.__init__

--- webdriver_test_tools/project/cmd/__init__.py
+++ webdriver_test_tools/project/cmd/__init__.py
@@ -1,1 +1,11 @@
-from . import common, run, list, new
+"""Modules for handling test package commands
+
+.. toctree::
+
+    webdriver_test_tools.project.cmd.common
+    webdriver_test_tools.project.cmd.list
+    webdriver_test_tools.project.cmd.new
+    webdriver_test_tools.project.cmd.run
+
+"""
+
