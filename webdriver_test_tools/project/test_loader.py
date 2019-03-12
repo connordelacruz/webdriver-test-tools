895030d10a1d71c41287ba1ba992452622b4a1dc
Connor de la Cruz <connor.c.delacruz@gmail.com>
2019-03-12 15:05:41 -0500

    cleaned up long lines and simplified some functions in test_loader
    - Wrapped long lines in docstrings and function definitions
    - Added some inline comments for less intuitive code
    - Simplified load_webdriver_test_cases() and _is_valid_case()

--- webdriver_test_tools/project/test_loader.py
+++ webdriver_test_tools/project/test_loader.py
@@ -7,28 +7,36 @@ from webdriver_test_tools.testcase import *
 
 
 # TODO: wildcard support
-def load_project_tests(tests_module, test_module_names=None, test_class_names=None, skip_class_names=None):
-    """Returns a list of :class:`WebDriverTestCase <webdriver_test_tools.testcase.webdriver.WebDriverTestCase>`
-    subclasses from all submodules in a test project's tests/ directory
+def load_project_tests(tests_module,
+                       test_module_names=None, test_class_names=None, skip_class_names=None):
+    """Returns a list of :class:`WebDriverTestCase
+    <webdriver_test_tools.testcase.webdriver.WebDriverTestCase>` subclasses
+    from all submodules in a test project's tests/ directory
 
     :param tests_module: The module object for ``<test_project>.tests``
-    :param test_module_names: (Optional) List of test module names. Only load test cases from a submodule of
-        ``tests_module`` with the given names
-    :param test_class_names: (Optional) List of test class names. Only load test cases with these names
-    :param skip_class_names: (Optional) List of test class names. Skip test cases with these names
+    :param test_module_names: (Optional) List of test module names. Only load
+        test cases from a submodule of ``tests_module`` with the given names
+    :param test_class_names: (Optional) List of test class names. Only load
+        test cases with these names
+    :param skip_class_names: (Optional) List of test class names. Skip test
+        cases with these names
 
     :return: A list of test classes from all test modules
     """
     test_list = []
+    # Get a list of all attributes from tests_module (skipping pkgutil)
     tests_module_attributes = [
         attr for attr in dir(tests_module) if attr != 'pkgutil'
     ]
+    # Filter attributes based on test_module_names (if provided)
     if test_module_names is not None:
         tests_module_attributes = [
             name for name in tests_module_attributes if name in test_module_names
         ]
+    # Iterate through attributes
     for name in tests_module_attributes:
         obj = getattr(tests_module, name)
+        # If obj is a module, extend test_list with the results of load_webdriver_test_cases()
         if isinstance(obj, types.ModuleType):
             test_list.extend(
                 load_webdriver_test_cases(obj, test_class_names, skip_class_names)
@@ -36,23 +44,25 @@ def load_project_tests(tests_module, test_module_names=None, test_class_names=No
     return test_list
 
 
-def load_webdriver_test_cases(module, test_class_names=None, skip_class_names=None):
-    """Returns a list of :class:`WebDriverTestCase <webdriver_test_tools.testcase.webdriver.WebDriverTestCase>`
-    subclasses from a module
+# TODO: get full class list first, then filter
+def load_webdriver_test_cases(module,
+                              test_class_names=None, skip_class_names=None):
+    """Returns a list of :class:`WebDriverTestCase
+    <webdriver_test_tools.testcase.webdriver.WebDriverTestCase>` subclasses
+    from a module
 
     :param module: The module to load tests from
-    :param test_class_names: (Optional) List of test case class names to load. Will load all if unspecified
-    :param skip_class_names: (Optional) List of test case class names to skip. Will skip none if unspecified
+    :param test_class_names: (Optional) List of test case class names to load.
+        Will load all if unspecified
+    :param skip_class_names: (Optional) List of test case class names to skip.
+        Will skip none if unspecified
 
     :return: A list of test classes loaded from the module
     """
-    test_list = []
-    for name in dir(module):
-        obj = getattr(module, name)
-        # Load class if it subclasses WebDriverTestCase (but don't load WebDriverTestCase itself)
-        if _is_valid_case(obj, test_class_names, skip_class_names):
-            test_list.append(obj)
-    return test_list
+    return [
+        getattr(module, name) for name in dir(module)
+        if _is_valid_case(getattr(module, name), test_class_names, skip_class_names)
+    ]
 
 
 def _is_valid_case(obj, test_class_names=None, skip_class_names=None):
@@ -77,23 +87,23 @@ def _is_valid_case(obj, test_class_names=None, skip_class_names=None):
     """
     parent_classes = [WebDriverTestCase, WebDriverMobileTestCase]
     # Load class if it subclasses WebDriverTestCase (but don't load WebDriverTestCase itself)
-    if (isinstance(obj, type) and issubclass(obj, WebDriverTestCase) and obj not in parent_classes
+    return (isinstance(obj, type) and issubclass(obj, WebDriverTestCase) and obj not in parent_classes
             and (skip_class_names is None or obj.__name__ not in skip_class_names)
-            and (test_class_names is None or obj.__name__ in test_class_names)
-    ):
-        return True
-    return False
+            and (test_class_names is None or obj.__name__ in test_class_names))
 
 
-def load_browser_tests(generated_test_cases, test_methods=None, skip_methods=None):
+# TODO: wildcard support for test functions
+def load_browser_tests(generated_test_cases,
+                       test_methods=None, skip_methods=None):
     """Load tests from browser test case classes
 
-    :param generated_test_cases: List of generated browser test case classes for a single subclass of
-        :class:`WebDriverTestCase <webdriver_test_tools.testcase.webdriver.WebDriverTestCase>`
-    :param test_methods: (Optional) List of test method names. If specified, load only these test methods from each
-        browser test case
-    :param skip_methods: (Optional) List of test method names. If specified, do not load these test methods from
-        each browser test case
+    :param generated_test_cases: List of generated browser test case classes
+        for a single subclass of :class:`WebDriverTestCase
+        <webdriver_test_tools.testcase.webdriver.WebDriverTestCase>`
+    :param test_methods: (Optional) List of test method names. If specified,
+        load only these test methods from each browser test case
+    :param skip_methods: (Optional) List of test method names. If specified, do
+        not load these test methods from each browser test case
 
     :return: A list of loaded tests from the browser test cases
     """
@@ -116,3 +126,4 @@ def load_browser_tests(generated_test_cases, test_methods=None, skip_methods=Non
             if test_method not in skip_methods
         ]
     return browser_tests
+
