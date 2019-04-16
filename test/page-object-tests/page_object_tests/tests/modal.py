b160f13fc6f370ad0c91a9638b284e2b917db2af
Connor de la Cruz <connor.c.delacruz@gmail.com>
2019-04-16 14:22:25 -0500

    Added tests for non-YAML modal objects

--- test/page-object-tests/page_object_tests/tests/modal.py
+++ test/page-object-tests/page_object_tests/tests/modal.py
@@ -3,7 +3,9 @@ import webdriver_test_tools
 from webdriver_test_tools.testcase import *
 
 from page_object_tests import config
+# TODO: rename to clarify it's using YAML
 from page_object_tests.pages.modal import ModalPage
+from page_object_tests.pages.no_yaml_modal import NoYAMLModalPage
 
 
 class ModalObjectTestCase(WebDriverTestCase):
@@ -14,9 +16,17 @@ class ModalObjectTestCase(WebDriverTestCase):
 
     # Test Methods
 
-    def test_modal_object(self):
-        """Test is_displayed() and click_close_button()"""
+    def test_modal_object_yaml(self):
+        """Test is_displayed() and click_close_button() (YAML)"""
         modal_page = ModalPage(self.driver)
+        self._test_modal_object(modal_page)
+
+    def test_modal_object_no_yaml(self):
+        """Test is_displayed() and click_close_button() (no YAML)"""
+        modal_page = NoYAMLModalPage(self.driver)
+        self._test_modal_object(modal_page)
+
+    def _test_modal_object(self, modal_page):
         modal_object = modal_page.click_open_modal_button()
         with self.subTest('Open modal'):
             self.assertTrue(modal_object.is_displayed())
