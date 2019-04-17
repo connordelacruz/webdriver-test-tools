182983990171c36fcc4b9ce31005aef1ddbe597b
Connor de la Cruz <connor.c.delacruz@gmail.com>
2019-04-17 12:16:39 -0500

    Added docstrings, click/hover return values

--- webdriver_test_tools/pageobject/nav.py
+++ webdriver_test_tools/pageobject/nav.py
@@ -8,10 +8,10 @@ from webdriver_test_tools.webdriver import actions
 # Link Page Objects
 
 class NavLinkObject(BasePage):
-    # TODO: doc and implement
+    """Page object prototype for nav links"""
 
     class ActionTypes:
-        # TODO: doc
+        """Link click/hover action types"""
         PAGE = 'page'
         SECTION = 'section'
         MENU = 'menu'
@@ -22,6 +22,10 @@ class NavLinkObject(BasePage):
         HOVER_ACTIONS = [
             MENU
         ]
+        # Required attributes for types
+        REQUIRES_TARGET = [
+            PAGE, SECTION
+        ]
 
     def __init__(self, driver, link_dict):
         # TODO: doc
@@ -30,15 +34,15 @@ class NavLinkObject(BasePage):
         # and raise errors otherwise
         self.name = link_dict['name']
         self.locator = utils.yaml.to_locator(link_dict['link_locator'])
-        # TODO: raise value errors if invalid
+        # TODO: raise value errors if invalid (allow either to be None though)
         self.click_action = link_dict.get('click', self.ActionTypes.PAGE)
         self.hover_action = link_dict.get('hover', None)
-        # TODO: raise key error if click_action is page/section
-        self.target = link_dict.get('target', None)
+        # Get target attribute if required
+        if self.click_action in self.ActionTypes.REQUIRES_TARGET:
+            self.target = link_dict['target']
         # Parse menu if applicable
         if self.ActionTypes.MENU in [self.click_action, self.hover_action]:
-            # TODO: self.menu = NavMenuObject(self.driver, link_dict['menu'])
-            pass
+            self.menu = NavMenuObject(self.driver, link_dict['menu'])
 
     # WebElement retrieval
 
@@ -54,21 +58,32 @@ class NavLinkObject(BasePage):
     # Actions
 
     def click_link(self):
+        """Click the link
+
+        :return: Link target (url or section ID) if ``self.click_action`` is
+            'page' or 'section', or a :class:`NavMenuObject` instance if
+            ``self.click_action`` is 'menu'
+        """
         actions.scroll.to_and_click(self.driver, self.find_link_element(), False)
-        # TODO: return something? (and doc)
+        return self.menu if self.click_action == self.ActionTypes.MENU else self.target
 
     def hover_over_link(self):
+        """Hover over the link element
+
+        :return: :class:`NavMenuObject` instance (or ``None`` if no hover
+            action is defined)
+        """
         link_element = self.find_link_element()
         actions.scroll.into_view(self.driver, link_element, False)
         action_chain = ActionChains(self.driver)
         action_chain.move_to_element(link_element).perform()
-        # TODO: return menu object? (and doc)
+        return self.menu if self.hover_action == self.ActionTypes.MENU else None
 
 
 # Menu Page Objects
 
 class NavMenuObject(BasePage):
-    # TODO: doc and implement
+    """Page object prototype for dropdown/collapsible nav menus"""
 
     def __init__(self, driver, menu_dict):
         # TODO: doc
@@ -98,12 +113,30 @@ class NavMenuObject(BasePage):
     # Actions
 
     def click_link(self, link_name):
-        # TODO: doc, return something, errors?
-        self.links[link_name].click_link()
+        """Click a link in the menu
+
+        :param link_name: Name of the link (specified in YAML or link
+            dictionary) i.e. a valid key in ``self.links``
+
+        :return: The returned value of clicking the link. See
+            :meth:`NavLinkObject.click_link` for possible values
+        """
+        # TODO: KeyError message?
+        return self.links[link_name].click_link()
 
     def hover_over_link(self, link_name):
-        # TODO: doc, return something, errors?
-        self.links[link_name].hover_over_link()
+        """Hover over a link in the menu
+
+        :param link_name: Name of the link (specified in YAML or link
+            dictionary) i.e. a valid key in ``self.links``
+
+        :return: The returned value of hovering over the link. See
+            :meth:`NavLinkObject.hover_over_link` for possible values
+        """
+        # TODO: KeyError message?
+        return self.links[link_name].hover_over_link()
+
+    # TODO: is_visible()
 
 
 # Navbar Page Objects
@@ -124,6 +157,7 @@ class NavObject(BasePage):
         scroll the target link into view before interacting with it
     """
 
+    # TODO: deprecate workflow
     # Link maps
     LINK_MAP = {}
     HOVER_MAP = {}
