89d9e33987c3e1860847bcf6e1e4a140a4d682d6
Connor de la Cruz <connor.c.delacruz@gmail.com>
2019-05-03 15:42:42 -0500

    the list command now shows what module each class is in

--- webdriver_test_tools/project/cmd/list.py
+++ webdriver_test_tools/project/cmd/list.py
@@ -20,7 +20,6 @@ def add_list_subparser(subparsers, parents=[],
     """
     list_description = 'Print a list of available tests and exit'
     list_help = list_description
-    # TODO: aliases=['ls']
     list_parser = subparsers.add_parser(
         'list', description=list_description, help=list_help,
         parents=parents,  # TODO: always use test_parent_parser?
@@ -42,21 +41,46 @@ def parse_list_args(tests_module, args):
     list_tests(tests_module, **kwargs)
 
 
+# TODO: params for verbosity
 def list_tests(tests_module,
                test_module_names=None, test_class_map=None, skip_class_map=None):
     """Print a list of available tests
 
     :param tests_module: The module object for ``<test_project>.tests``
-    :param test_module_names: (Optional) Parsed arg for ``--module`` command line
-        argument
-    :param test_class_map: (Optional) Result of passing parsed arg for ``--test``
-        command line argument to :func:`parse_test_names()`
-    :param skip_class_map: (Optional) Result of passing parsed arg for ``--skip``
-        command line argument to :func:`parse_test_names()`
+    :param test_module_names: (Optional) Parsed arg for ``--module`` command
+        line argument
+    :param test_class_map: (Optional) Result of passing parsed arg for
+        ``--test`` command line argument to :func:`parse_test_names()`
+    :param skip_class_map: (Optional) Result of passing parsed arg for
+        ``--skip`` command line argument to :func:`parse_test_names()`
     """
     tests = load_tests(tests_module, test_module_names, test_class_map, skip_class_map)
-    for test_class in tests:
-        print(cmd.COLORS['title'](test_class.__name__) + ':')
-        test_cases = unittest.loader.getTestCaseNames(test_class, 'test')
-        for test_case in test_cases:
-            print(textwrap.indent(test_case, cmd.INDENT))
+    module_map = _module_map(tests, tests_module)
+    for module, test_list in module_map.items():
+        print(cmd.COLORS['prompt'](module) + ':')
+        for test_class in test_list:
+            print(textwrap.indent(cmd.COLORS['title'](test_class.__name__) + ':', cmd.INDENT))
+            test_cases = unittest.loader.getTestCaseNames(test_class, 'test')
+            print(*[
+                textwrap.indent(test_case, cmd.INDENT * 2) for test_case in test_cases
+            ], sep='\n')
+
+
+def _module_map(tests, tests_module):
+    """Returns a dictionary mapping test module names to a list of the test
+    case classes in the module
+
+    :param tests: List of test case classes
+    :param tests_module: The module object for ``<test_project>.tests``
+
+    :return: Dictionary mapping test module names to a list of the test
+        case classes in the module
+    """
+    module_prefix = tests_module.__name__ + '.'
+    module_map = {}
+    for test in tests:
+        module = test.__module__.replace(module_prefix, '')
+        if module not in module_map:
+            module_map[module] = []
+        module_map[module].append(test)
+    return module_map
